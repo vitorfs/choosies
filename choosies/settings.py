72b1b8b694cddd06a12779c8dbba91ac37f6a29a
Vitor Freitas <vitorfs@gmail.com>
2014-03-12 18:31:32 -0300

    changing settings.py

--- choosies/settings.py
+++ choosies/settings.py
@@ -10,7 +10,11 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {
+  'default': dj_database_url.config(
+    default = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
